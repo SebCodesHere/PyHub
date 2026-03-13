3ccfcf634c95454449de7b87c348e43457b06733
Seb <sebastiankocur21@gmail.com>
2026-03-13 22:05:21 +0100

    Update

--- pyhub.py
+++ pyhub.py
@@ -1,52 +1,63 @@
-#!/usr/bin/env python3
-import os
 import pyfiglet
-from colorama import init, Fore, Style
-from tools import web_status, calculator, base64_tool, speed_test, port_scanner, temp_nuker
+from colorama import Fore, Style, init
+from tools import web_status, calculator, base64_tool, speed_test, port_scanner, temp_nuker, ping_tool
 
-# Initialize colorama
 init(autoreset=True)
 
-# Print colored ASCII logo
-logo = pyfiglet.figlet_format("PyHub")
-print(Fore.BLUE + logo)
-print(Fore.WHITE + Style.BRIGHT + "Your Python CLI Toolkit\n")
+def logo():
+    text = pyfiglet.figlet_format("PyHub")
+    print(Fore.BLUE + text)
+
+def menu():
+    print(Fore.CYAN + "Select a tool:\n")
+
+    print(Fore.YELLOW + "1." + Style.RESET_ALL + " Web Status Checker")
+    print(Fore.YELLOW + "2." + Style.RESET_ALL + " Calculator")
+    print(Fore.YELLOW + "3." + Style.RESET_ALL + " Base64 Encoder/Decoder")
+    print(Fore.YELLOW + "4." + Style.RESET_ALL + " Speed Test")
+    print(Fore.YELLOW + "5." + Style.RESET_ALL + " Local Port Scanner")
+    print(Fore.YELLOW + "6." + Style.RESET_ALL + " Temp File Nuker")
+    print(Fore.YELLOW + "7." + Style.RESET_ALL + " Ping Tool")
+    print(Fore.RED + "0." + Style.RESET_ALL + " Exit")
 
 def main():
-    try:
-        while True:
-            # Colored menu
-            print(Fore.CYAN + "--- PyHub Toolkit ---")
-            print(Fore.GREEN + "1. Web Status Checker")
-            print(Fore.YELLOW + "2. Calculator")
-            print(Fore.MAGENTA + "3. Base64 Encoder/Decoder")
-            print(Fore.BLUE + "4. Speed Test")
-            print(Fore.RED + "5. Local Port Scanner")
-            print(Fore.LIGHTBLACK_EX + "6. Temp File Nuker")
-            print(Fore.WHITE + "7. Exit\n")
+    while True:
+        logo()
+        menu()
 
-            choice = input(Fore.CYAN + "Choose an option: " + Fore.WHITE)
+        choice = input(Fore.GREEN + "\nChoose an option: ")
 
-            if choice == "1":
-                web_status.run()
-            elif choice == "2":
-                calculator.run()
-            elif choice == "3":
-                base64_tool.run()
-            elif choice == "4":
-                speed_test.run()
-            elif choice == "5":
-                port_scanner.run()
-            elif choice == "6":
-                temp_nuker.run()
-            elif choice == "7":
-                print(Fore.CYAN + "Exiting PyHub...")
-                break
-            else:
-                print(Fore.RED + "Invalid option!")
+        if choice == "1":
+            web_status.run()
 
-    except KeyboardInterrupt:
-        print(Fore.CYAN + "\nExiting PyHub...")
+        elif choice == "2":
+            calculator.run()
+
+        elif choice == "3":
+            base64_tool.run()
+
+        elif choice == "4":
+            speed_test.run()
+
+        elif choice == "5":
+            port_scanner.run()
+
+        elif choice == "6":
+            temp_nuker.run()
+
+        elif choice == "7":
+            ping_tool.run()
+
+        elif choice == "0":
+            print(Fore.RED + "\nExiting PyHub...")
+            break
+
+        else:
+            print(Fore.RED + "Invalid option")
+
+        input(Fore.MAGENTA + "\nPress Enter to return to menu...")
+        print("\n" * 2)
+
 
 if __name__ == "__main__":
     main()
