1618443407e82e30ffe47cda0b24964ac5bb98fa
Seb <sebastiankocur21@gmail.com>
2026-03-20 20:52:07 +0100

    fix

--- tools/rann_gen.py
+++ tools/rann_gen.py
@@ -1,18 +1,19 @@
 import random
 
+def run():
 # Colors
-blue = "\033[94m"
-red = "\033[91m"
-reset = "\033[0m"
+    blue = "\033[94m"
+    red = "\033[91m"
+    reset = "\033[0m"
 
-# Colored questions
-digits = int(input(blue + "How many digits should the number have? " + reset))
-times = int(input(red + "How many times to print? " + reset))
 
-# Generate numbers
-min_num = 10**(digits - 1)
-max_num = (10**digits) - 1
+    digits = int(input(blue + "How many digits should the number have? " + reset))
+    times = int(input(red + "How many times to print? " + reset))
 
-for _ in range(times):
-    num = random.randint(min_num, max_num)
-    print(num)
+    # Generate numbers
+    min_num = 10**(digits - 1)
+    max_num = (10**digits) - 1
+
+    for _ in range(times):
+        num = random.randint(min_num, max_num)
+        print(num)
